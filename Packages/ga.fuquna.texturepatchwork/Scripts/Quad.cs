a1c68408bc7425cd464fb6cd5d005261b12d52cf
fuqunaga <fukhid@gmail.com>
2023-06-15 18:55:43 +0900

    Quad implicit cast from Rect

--- Packages/ga.fuquna.texturepatchwork/Scripts/Quad.cs
+++ Packages/ga.fuquna.texturepatchwork/Scripts/Quad.cs
@@ -13,5 +13,13 @@ namespace TexturePatchwork
         public Vector2 leftTop = Vector2.up;
         public Vector2 rightBottom = Vector2.right;
         public Vector2 rightTop = Vector2.one;
+
+        public static implicit operator Quad(Rect rect) => new()
+        {
+            leftBottom = rect.min,
+            leftTop = new Vector2(rect.xMin, rect.yMax),
+            rightBottom = new Vector2(rect.xMax, rect.yMin),
+            rightTop = rect.max
+        };
     }
 }
